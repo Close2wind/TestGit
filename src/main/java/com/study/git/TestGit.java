95bdf8310955896bb3d961999f14b5c205511f7f
eclipse_user <eclipse_user@study.com>
2019-09-10 10:56:02 +0800

    commit hot fix

--- src/main/java/com/study/git/TestGit.java
+++ src/main/java/com/study/git/TestGit.java
@@ -1,5 +1,8 @@
 package com.study.git;
 
 public class TestGit {
-
+	public static void main(String[] args) {
+		System.out.println("good!very!");
+		System.out.println("hot_fix..............");
+	}
 }
